8d65e76f4d1521513e46afe2a9ab7b5a6f10a5d0
sagar <sshingaspure@gmail.com>
2016-12-04 01:02:36 +0530

    added mail project

---
+++ CreateMail/src/com/mainApp/MainApp.java
@@ -0,0 +1,139 @@
+package com.mainApp;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class MainApp {
+	public static void main(String[] args) {
+		//ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		
+		List<Map<String, String>> statistics=null;
+		try {
+			statistics = getFilesStats();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+		String numOfFiles=""+statistics.size();
+		StringBuilder table=new StringBuilder();
+		StringBuilder stats=new StringBuilder();
+		
+		for (Map<String, String> map : statistics) {
+			table.append(map.get("FileName").split("/")[6]+"\t"+map.get("Total Record")+"\t"+"File looks good"+"\n");
+			stats.append("FileName: "+map.get("FileName")+"\n"+"Total Record: "+map.get("Total Record")+"\n"+"Success Record: "+map.get("Success Record")
+					+"\n"+"Record Rejected: "+map.get("Record Rejected")+"\n\n"+"Error File: /home/bconnected/bc3_import/IBC_Reform/LiveVoxDailyResultLoad/error_"
+					+map.get("FileName").split("/")[6]+".processed"+"\n\n");
+		}
+		
+		StringBuilder dailyExtractFiles=new StringBuilder();
+		StringBuilder noteExtractFiles=new StringBuilder();
+		StringBuilder dates=new StringBuilder();
+		
+		Set<String> set=new HashSet<String>();
+		
+		for (Map<String, String> map : statistics) {
+			String fileName=map.get("FileName").split("/")[6];
+			String datePortion=fileName.split("_")[3];
+			set.add(datePortion);
+		}
+		
+		int count=1;
+		for (String string : set) {
+			dailyExtractFiles.append(count+") "+"/home/bconnected/bc3_export/IBC_Reform/DailyTransactionFeed/IBC_AHNJ_Daily_Transaction_File_"+string+".txt"+"\n");
+			noteExtractFiles.append(count+") "+"/home/bconnected/bc3_export/IBC_Reform/NoteExtract/Note_Extract_"+string+".txt"+"\n");
+			if (count==set.size()-1) {
+				dates.append(string.substring(0, 2)+"/"+string.substring(2, 4)+" and ");	
+			}else if(count==set.size()){
+				dates.append(string.substring(0, 2)+"/"+string.substring(2, 4));
+			}else{
+				dates.append(string.substring(0, 2)+"/"+string.substring(2, 4)+",");
+			}
+			
+			count++;
+		}
+		
+		/*System.out.println(numOfFiles);
+		System.out.println(table);
+		System.out.println(stats);
+		System.out.println(dailyExtractFiles);
+		System.out.println(noteExtractFiles);
+		System.out.println(dates);
+		 */
+		
+		BufferedReader reader=null;
+		StringBuilder templateData=new StringBuilder();
+		try {
+			reader=new BufferedReader(new FileReader("template.txt"));
+			String line=new String();
+			while((line=reader.readLine())!=null){
+				templateData.append(line);
+				templateData.append("\n");
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String mailData=new String(templateData);
+		mailData=mailData.replaceFirst("<<numOfFiles>>", numOfFiles);
+		mailData=mailData.replaceFirst("<<table>>", new String(table).trim());
+		mailData=mailData.replaceFirst("<<stats>>", new String(stats).trim());
+		mailData=mailData.replaceFirst("<<dailyExtractFiles>>", new String(dailyExtractFiles).trim());
+		mailData=mailData.replaceFirst("<<noteExtractFiles>>", new String(noteExtractFiles).trim());
+		mailData=mailData.replaceAll("<<dates>>", new String(dates));
+		
+		System.out.println(mailData);
+		
+		
+	}
+
+	private static List<Map<String, String>> getFilesStats() throws IOException {
+
+		List<Map<String, String>> statistics = new LinkedList<Map<String, String>>();
+
+		HashMap<String, String> map = new HashMap<String, String>();
+		BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+		String line = new String();
+		while ((line = reader.readLine()) != null) {
+			switch (line) {
+			case "EOF":
+				return statistics;
+
+			case "\n":
+				statistics.add(map);
+				map = new HashMap<String, String>();
+				break;
+				
+			default:
+				String[] tokens=line.split(":");
+				if (tokens.length<2) {
+					statistics.add(map);
+					map = new HashMap<String, String>();
+					break;
+				}
+				map.put(tokens[0].trim(), tokens[1].trim());
+				
+			}
+
+		}
+		
+		return statistics;
+	}
+
+}
